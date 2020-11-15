bde521932d08e468d6bf54c01afb8ef36024d260
Geoff Nix <FullStackGeoff@gmail.com>
2020-11-15 11:48:43 -0500

    aded heroku

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.listen(PORT, function () {
-    console.log(`App listening on port http://${PORT}`);
+	console.log(`App listening on port http://${PORT}`);
+});
 
-})
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "/assets/js/index.html"));
+});
+
+app.get("/api/config", (req, res) => {
+	res.json({
+		success: true,
+	});
+});
